ebecc4eed6c9eddbfc27b1bb04b17eae91719825
Rahul Dey <rahul900day@gmail.com>
2020-07-06 00:50:22 +0530

    Initial commit

---
+++ installer.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#Author: Rahul Dey
+#GitHub: https://github.com/RahulDey12
+
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+
+echo "Apache2 & Mysql is Installing..."
+apt install apache2 mysql-server -y
+
+echo "Starting Apache & Mysql"
+service apache2 start
+service mysql start
+
+echo "Initializing PHP Installation"
+apt-get install software-properties-common -y
+add-apt-repository ppa:ondrej/php
+apt-get update
+
+echo "PHP 7.3 Modules Installing ..."
+apt install php7.3 -y
+apt install php7.3-cli php7.3-xml php7.3-mysql php7.3-mbstring php7.3-bcmath php7.3-bz2 php7.3-bcmath php7.3-curl php7.3-gd php7.3-zip -y
+
+echo "PHP 7.4 Modules Installing ..."
+apt install php7.4 -y
+apt install php7.4-cli php7.4-xml php7.4-mysql php7.4-mbstring php7.4-bcmath php7.4-bz2 php7.4-bcmath php7.4-curl php7.4-gd php7.4-zip -y
+
+echo "Setting Up Default PHP version to 7.3"
+update-alternatives --set php /usr/bin/php7.3
+a2dismod php7.4
+a2enmod php7.3
+service apache2 restart
+
+echo "Composer is Installing..."
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+#Moving Composer From Current Path
+mv $(pwd)/composer.phar /usr/bin/composer.phar
+alias composer='composer.phar'
+
+echo "PhpMyadmin is Installing..."
+apt install phpmyadmin -y
+
---
+++ instructions.md
@@ -0,0 +1,42 @@
+# Follow This Steps After Running installer.sh
+
+#### Create A Directory on Your `C://` Drive Called `server/www`
+
+### Edit Lines in `/etc/apache2/sites-available/000-default.conf`
+```
+DocumentRoot /mnt/c/server/www
+```
+
+### Edit Lines in `/etc/apache2/apache2.conf`
+```
+<Directory />
+        Options Indexes FollowSymLinks Includes ExecCGI
+        AllowOverride All
+        Require all granted
+</Directory>
+
+```
+
+### Add Lines to `~/.bashrc`
+```
+alias composer='composer.phar'
+export PATH=$PATH:~/.config/composer/vendor
+```
+
+### Add Lines to `/etc/mysql/my.cnf`
+```
+[mysqld]
+default_authentication_plugin = mysql_native_password
+```
+
+### Run Commands
+```
+$ sudo mysql
+mysql> CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
+mysql> GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
+mysql> FLUSH PRIVILEGES;
+mysql> exit
+
+ln -s /usr/share/phpmyadmin /mnt/c/server/www/phpmyadmin
+
+```
